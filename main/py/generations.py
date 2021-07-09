4d2ce6c57f1cb9d0c5e02910de04b80916fac283
Fennec Nightingale <fenneccharles@gmail.com>
2021-07-09 02:41:40 -0700

    troubleshooting end of website, once thats done just need to add visuals and finish readme

--- main/py/generations.py
+++ main/py/generations.py
@@ -4,6 +4,7 @@ import json
 import random 
 import tarfile
 
+from pathlib import Path
 import tensorflow as tf
 import gpt_2_simple as gpt2
 
@@ -14,6 +15,8 @@ from gpt_2_simple.src import model
 
 def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
     start = datetime.now()
+    # print('downloading model')
+    # gpt2.download_gpt2(model_dir='models', model_name='774M')
     # start temporary list to hold our output text
     templist = []
     # generate text for each act 
@@ -25,29 +28,34 @@ def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
                 sess = gpt2.reset_session(sess)
         except:
             sess = gpt2.start_tf_sess()
-        # print('opening TAR')
-        with tarfile.open(f"../..tars/checkpoint_{i}.tar", 'r') as tar:
+        # loaded this function up with print statments, so that if something 
+        # goes wrong but doesn't have a clear error message, we can still
+        # debug exactly where it's happening 
+        print('gathering TAR directory')
+        p = str(Path(__file__).parents[2]).replace('\\', '/')
+        print('opening TAR')
+        with tarfile.open(f"{p}/tars/checkpoint_{i}.tar", 'r') as tar:
             tar.extractall(os.path.join('models', '774M', '.checkpoint'))
-        # print('opening checkpoint path')
+        print('opening checkpoint path')
         checkpoint_path = os.path.join('models', '774M')
-        # print('setting default hparams')
+        print('setting default hparams')
         hparams = model.default_hparams()
-        # print('loading default hparams')
+        print('loading default hparams')
         with open(os.path.join(checkpoint_path, 'hparams.json')) as f:
             hparams.override_from_dict(json.load(f))
-        # print('whatever the fuck context is doing ')
+        print('whatever context is doing ')
         context = tf.compat.v1.placeholder(tf.int32, [1, None])
-        # print('setting output')
+        print('setting output')
         output = model.model(hparams=hparams, X=context)
-        # print('loading latest checkpoint')
+        print('loading latest checkpoint')
         ckpt = tf.train.latest_checkpoint(checkpoint_path)
-        # print('setting memory saver')
+        print('setting memory saver')
         saver = tf.compat.v1.train.Saver(allow_empty=True)
-        # print('starting the session')
+        print('starting the session')
         sess.run(tf.compat.v1.global_variables_initializer())
-        # print('restoring last known variables')
+        print('restoring last known variables')
         saver.restore(sess, ckpt)
-        # print('determining scene length')
+        print('determining scene length')
         # determine how much we need to generate 
         if LENGTH == 'SCENE':
             length = 100
@@ -58,7 +66,7 @@ def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
         else:
             length = 400
             rng = 4
-        # print('start generating')
+        print('start generating')
         # start generating            
         for j in range(rng):
             txt = ''
@@ -69,33 +77,25 @@ def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
             # otherwise have it grab the end of the last generated text
             else:
                 prefix = '\n'.join(templist[-500:])
-            # print('adding our characters')
-            # on even generations we'll generate A plot scenes
-            # if LENGTH != 'SHORT':
-            #     alst = [0, 2]
-            #     blst = [1, 3]
-            # else:
-            #     alst = [0]
-            #     blst = [2]
-            if j == 0 and len(a_chars_in_location) > 0:#and k in alst:
+            if j == 0 and len(a_chars_in_location) > 0:
                 prefix = prefix + str(a_chars_in_location.pop(0))
                 prefix = ' '.join(prefix.split(' ')[-500:])
                 if len(templist) == 0:
                     templist.append(prefix)
             # on even generations we'll generate A plot scenes
-            elif j == 1 and len(b_chars_in_location) > 0:# and k in blst:
+            elif j == 1 and len(b_chars_in_location) > 0:
                 print(prefix)
                 prefix = prefix + str(b_chars_in_location.pop(0))
                 prefix = ' '.join(prefix.split(' ')[-500:])
-            # print('generating and putting our text into a list')
+            print('generating and putting our text into a list')
             # append our generated text to our list for storage 
-            length = min((length + len(prefix)), 1000)
+            final_len = min(((length*(j+1)) + len(prefix.split(' '))), 1000)
             txt += gpt2.generate(sess,
                                 run_name = i,
-                                length = length,
-                                temperature = .01,
-                                top_k = 100, 
-                                top_p= .50,
+                                length = final_len,
+                                temperature = 1.5,
+                                top_k = 40, 
+                                top_p= .95,
                                 prefix = prefix,
                                 nsamples = 1,
                                 include_prefix = False,
@@ -109,8 +109,8 @@ def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
             txt = txt.replace('</b>', '').replace('*', '').replace('(x2)', '')
             txt = txt.replace('>', '').replace('<', '').replace('.', '.\n')
             txt = txt.replace('!', '!\n').replace('?', '?\n').replace('  ', ' ')
-            txt = txt.replace(' .', '.').replace(' ,', ',').replace(' !', '!')
-            txt = txt.replace(' ?', '?')
+            txt = txt.replace(' ,', ',')
+            txt = txt.replace('/n', ' /n ').replace('  ', ' ')
             for word in txt.split(' '):
                 # print(word)
                 if ':' in word and word != 'TREK:':
@@ -120,22 +120,14 @@ def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
                     else:
                         txt = txt.replace(word, f'\n{word}')
             for l in txt.split('\n'):
-                # print(l)
                 if len(templist) > 0:
                     len_ = len(set(re.sub(r'[^\w\s]', '', l).strip().split(' ')))
                     new_ = len(set(re.sub(r'[^\w\s]', '', l + templist[-1]).strip().split(' ')))
-                    print('len' + str(len_))
-                    print('new' + str(new_))
                 else:
                     len_ = 0
                     new_ = 10
-                    # print('len' + len_)
-                    # print('new' + new_)
                 if l not in templist and 'written by' not in l.lower() and new_-len_ >= 2:
-                    # print(l)
                     templist.append(l)
-            k += 1
-    # print('list to text')
     text = '\n'.join(templist).replace('\n', '<br>')
     for i in range(3):
         text = text.replace('<br><br>', '<br>')
