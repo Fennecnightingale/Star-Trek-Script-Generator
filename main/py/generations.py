9eaebf248893e5fde7e97237be248131a457566f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-20 17:52:14 +0000

    Adding tarfile member sanitization to extractall()

--- main/py/generations.py
+++ main/py/generations.py
@@ -40,7 +40,26 @@ def generate(a_chars_in_location, b_chars_in_location, SERIES, LENGTH):
         p = str(Path(__file__).parents[2]).replace('\\', '/')
         print('opening TAR')
         with tarfile.open(f"{p}/tars/checkpoint_{i}.tar", 'r') as tar:
-            tar.extractall(os.path.join('models', '1558M', '.checkpoint'))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, os.path.join("models","1558M",".checkpoint"))
         print('opening checkpoint path')
         checkpoint_path = os.path.join('models', '1558M')
         print('setting default hparams')
